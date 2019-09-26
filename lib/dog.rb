ca93e80e67e7f039f6c7cb76a1569b8b75dad631
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-26 20:18:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog 
-  def name= #getter 
+  def name=(full_name) #setter 
+    first_name, last_name = full_name.split
+    @first_name = first_name
+    @last_name = last_name
+  end
+  
+  
+  def name #getter
+    #{@first_name} #{last_name}".strip
+  end
+    
 end
